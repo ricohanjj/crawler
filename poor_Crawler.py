e9c4468066742411fab88ad39ccb3571e6c00ec5
ricohanjj <ricohanjj@163.com>
2018-11-07 15:10:14 +0800

    Poor Crawler Done

--- poor_Crawler.py
+++ poor_Crawler.py
@@ -12,4 +12,32 @@ with open("alexa.txt",'r') as f:
 
 start = time.time()
 def crawler(q,index):
-    Process_id = 'Process- '+index
+    Process_id = 'Process- '+str(index)
+    while not q.empty():
+        url = q.get(timeout=20)
+        try:
+            r = requests.get(url,timeout=20)
+            print(Process_id,q.qsize(),r.status_code,url)
+        except Exception as e:
+            print(Process_id,q.qsize(),url,'Error:',e)
+
+if __name__ == '__main__':
+    manager = Manager()
+    workQueue = manager.Queue(1000)
+
+    for url in link_list:
+        workQueue.put(url)
+
+    pool = Pool(processes=3)
+    for i in range(4):
+        # print(i)
+        pool.apply_async(crawler,args=(workQueue,i))
+    #
+    print("Start Processes")
+    pool.close()
+    pool.join()
+
+    print('Main Process quit')
+
+
+
